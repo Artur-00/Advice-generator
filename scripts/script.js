695cfde7a464e3c93370e587d5464e6f7945b415
Artur-00 <96730639+Artur-00@users.noreply.github.com>
2022-12-06 15:29:00 +0000

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+
+const adviceIdElement = document.querySelector('.advice-id span');
+const adviceReceiverElement = document.querySelector('.advice-receiver');
+const generatorButton = document.querySelector('.button-background');
+
+const getAdvice = async () => {
+    try {
+        fetch('https://api.adviceslip.com/advice', {cache: "no-cache"})
+        .then((response) => response.json())
+        .then((json) => changeElements(json));
+    } catch (error) {
+        console.log(error);
+        return new Error(error);
+    }
+}
+
+getAdvice();
+
+function changeElements(json) {
+    const advice = json.slip;
+    
+    adviceIdElement.innerHTML = `#${advice.id}`;
+    adviceReceiverElement.innerHTML = `"${advice.advice}"`;
+}
+
+generatorButton.addEventListener("click", getAdvice);
